327c5977f3f08422a9dc9650442c1708578797d9
Federico Almada <55369229+FeeDeeX46@users.noreply.github.com>
2019-12-05 17:20:12 -0300

    Add files via upload

---
+++ workspace/speaker_tone/Debug/src/main.d
@@ -0,0 +1,25 @@
+src/main.o src/main.d: ../src/main.c \
+ C:\Users\FeeDeeX46\Documents\LPCXpresso_8.2.2_650\workspace\Lib_MCU\inc/lpc17xx_pinsel.h \
+ C:\Users\FeeDeeX46\Documents\LPCXpresso_8.2.2_650\workspace\Lib_CMSISv1p30_LPC17xx\inc/LPC17xx.h \
+ C:\Users\FeeDeeX46\Documents\LPCXpresso_8.2.2_650\workspace\Lib_CMSISv1p30_LPC17xx\inc/core_cm3.h \
+ C:\Users\FeeDeeX46\Documents\LPCXpresso_8.2.2_650\workspace\Lib_CMSISv1p30_LPC17xx\inc/system_LPC17xx.h \
+ C:\Users\FeeDeeX46\Documents\LPCXpresso_8.2.2_650\workspace\Lib_MCU\inc/lpc_types.h \
+ C:\Users\FeeDeeX46\Documents\LPCXpresso_8.2.2_650\workspace\Lib_MCU\inc/lpc17xx_gpio.h \
+ C:\Users\FeeDeeX46\Documents\LPCXpresso_8.2.2_650\workspace\Lib_MCU\inc/lpc17xx_timer.h \
+ C:\Users\FeeDeeX46\Documents\LPCXpresso_8.2.2_650\workspace\Lib_EaBaseBoard\inc/joystick.h
+
+C:\Users\FeeDeeX46\Documents\LPCXpresso_8.2.2_650\workspace\Lib_MCU\inc/lpc17xx_pinsel.h:
+
+C:\Users\FeeDeeX46\Documents\LPCXpresso_8.2.2_650\workspace\Lib_CMSISv1p30_LPC17xx\inc/LPC17xx.h:
+
+C:\Users\FeeDeeX46\Documents\LPCXpresso_8.2.2_650\workspace\Lib_CMSISv1p30_LPC17xx\inc/core_cm3.h:
+
+C:\Users\FeeDeeX46\Documents\LPCXpresso_8.2.2_650\workspace\Lib_CMSISv1p30_LPC17xx\inc/system_LPC17xx.h:
+
+C:\Users\FeeDeeX46\Documents\LPCXpresso_8.2.2_650\workspace\Lib_MCU\inc/lpc_types.h:
+
+C:\Users\FeeDeeX46\Documents\LPCXpresso_8.2.2_650\workspace\Lib_MCU\inc/lpc17xx_gpio.h:
+
+C:\Users\FeeDeeX46\Documents\LPCXpresso_8.2.2_650\workspace\Lib_MCU\inc/lpc17xx_timer.h:
+
+C:\Users\FeeDeeX46\Documents\LPCXpresso_8.2.2_650\workspace\Lib_EaBaseBoard\inc/joystick.h:
